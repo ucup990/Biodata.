2a632c3dbfea270eb5d09a7506ef16a5a2ea750c
Mohd Yusuf Ramdhani <60150914+ucup990@users.noreply.github.com>
2021-10-13 11:29:49 +0700

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,16 @@
+public class biodata {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Program Biodata");
+        System.out.println("===============");
+        System.out.println("Nama: Mohd Yusuf Ramdahni");
+        System.out.println("Alamat: Tanjungpinang");
+        System.out.println("No Telp: 081271686799");
+        System.out.println("Jurusan : Teknik Informatika");
+        System.out.println("Kegiatan : NDP Batam");
+        System.out.println("Program Selesai");
+        System.out.println("===============");
+        
+    }
+}
